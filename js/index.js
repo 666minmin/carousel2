27e48e2ddd204dc0ae79e142d8151b62b79dffb2
mmc <864604538@qq.com>
2018-04-01 09:25:43 +0800

    无缝连接轮播

---
+++ js/index.js
@@ -0,0 +1,38 @@
+var n=1;
+initStatus(n);
+setInterval(function(e){
+    makeLeaveStatus(getImageItem(n))
+    .one("transitionend",function(e){
+        makeEnterStatus($(e.currentTarget));
+    });
+    makeCurrentStatus(getImageItem(n+1));
+    n+=1;
+    n=changeN(n);
+},3000)
+
+function initStatus(n){
+    getImageItem(n).addClass("current")
+    .siblings().add("enter");
+}
+function changeN(n){
+    var max=$(".images>img").length;
+    return n%max===0?max:n%max;
+}
+function makeCurrentStatus($node){
+    $node.removeClass("leave enter").addClass("current");
+    return $node;
+}
+function makeLeaveStatus($node){
+    $node.removeClass("enter current").addClass("leave");
+    return $node;
+ }
+ function makeEnterStatus($node){
+    $node.removeClass("current leave").addClass("enter");
+    return $node;
+ }
+ function getImageItem(n){
+     n=changeN(n);
+     return $(".images>img:nth-child("+n+")");
+ }
+
+
